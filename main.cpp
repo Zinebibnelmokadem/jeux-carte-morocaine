021e58b0ab72214bacd56f6f8a4729ae7b52f637
Zinebibnelmokadem <148863025+Zinebibnelmokadem@users.noreply.github.com>
2023-12-24 19:30:47 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QLocale>
-#include <QTranslator>
-#include <QWidget>
-#include <QVBoxLayout>
+
+
 #include <QLabel>
 
 int main(int argc, char *argv[])
